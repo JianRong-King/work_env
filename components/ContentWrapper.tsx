95707460b4d8e9c0434fcba901ed6876d108b3d8
King Jian Rong <kingjianrong3249@gmail.com>
2025-01-12 23:33:42 +0800

    Defined module structure

---
+++ components/ContentWrapper.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import SpofityPlayer from "./SpotifyPlayer";
+import PomotoTimer from "./PomotoTimer";
+
+export default function ContentWrapper() {
+    return (
+        <div>
+            <SpofityPlayer />
+            <PomotoTimer />
+        </div>
+    )
+}
---
+++ components/PomotoTimer.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function PomotoTimer(){
+    return (
+        <div className='border-2 min-h-64 m-[10%]' >
+            
+        </div>
+    )
+}
