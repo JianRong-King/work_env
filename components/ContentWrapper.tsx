6f6a7825f90bd2f59df0e15d6dfaee22d82f0d0d
King Jian Rong <kingjianrong3249@gmail.com>
2025-01-13 15:02:16 +0800

    Changed header text

--- components/ContentWrapper.tsx
+++ components/ContentWrapper.tsx
@@ -6,7 +6,7 @@ import TodoList from "./TodoList";
 export default function ContentWrapper() {
   return (
     <div>
-      <div className="flex flex-col gap-4  items-center my-10">
+      <div className="flex flex-col gap-4 items-center my-10">
         <PomotoTimer />
         <div className="flex flex-row m-6 p-6 ">
           <SpofityPlayer />
--- components/Header.tsx
+++ components/Header.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Header() {
   return (
     <header className="bg-gray-800 text-whitep-4 flex justify-center">
-      <h1 className="">Study With Me</h1>
+      <h1 className="">Study With King</h1>
     </header>
   );
 }
